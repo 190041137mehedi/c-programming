0cd1770d3db9cf0d3c1d3bfc0a24214548bd0558
190041137mehedI <190041137mehedi@gmail.com>
2023-04-08 14:00:38 +0600

    Module 3.5 practice

---
+++ Module 3.5 Practice/N time.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int i, n;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("I Love Practice\n", n);
+    }
+    return 0;
+}
---
+++ Module 3.5 Practice/Sum of Two Numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int A, B, SUM;
+    scanf("%d %d", &A, &B);
+    SUM = A + B;
+    printf("%d", SUM);
+    return 0;
+}
