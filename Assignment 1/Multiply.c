2eda219c562f47f16ecc944dee56d0d300a6f32c
190041137mehedI <190041137mehedi@gmail.com>
2023-04-08 14:25:51 +0600

    Assignment 1

---
+++ Assignment 1/Multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    long long int A, B, sum;
+    scanf("%lld %lld", &A, &B);
+    sum = A * B;
+    printf("%lld", sum);
+    return 0;
+}
